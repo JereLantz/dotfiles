3ef917bf9a076708f42dbfef65f37ef3fc17d07a
Jere Lantz <lantz.jere@gmail.com>
2025-09-21 20:28:47 +0300

    better rust formating command

--- nvim/after/ftplugin/rust.lua
+++ nvim/after/ftplugin/rust.lua
@@ -1,7 +1,3 @@
--- Run rustfmt on the current file on save
-vim.api.nvim_create_autocmd("BufWritePost", {
-    pattern = "*.rs",
-    callback = function()
-        vim.cmd("silent! !rustfmt $(echo %)")
-    end,
-})
+-- Run rustfmt for the current file
+-- [f]ormat [f]ile
+vim.keymap.set("n", "<leader>ff", "<cmd>silent! !rustfmt $(echo %)<CR>")
