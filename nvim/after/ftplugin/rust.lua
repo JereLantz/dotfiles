50d038417d93252e7b705fdc95a19ec33cc34113
Jere Lantz <lantz.jere@gmail.com>
2025-09-23 21:29:59 +0300

    fixed rust formatting command

--- nvim/after/ftplugin/rust.lua
+++ nvim/after/ftplugin/rust.lua
@@ -1,3 +1,7 @@
 -- Run rustfmt for the current file
 -- [f]ormat [f]ile
-vim.keymap.set("n", "<leader>ff", "<cmd>silent! !rustfmt $(echo %)<CR>")
+-- This doesn't work because rustfmt doesn't check the rust version from the cargo.toml file
+-- vim.keymap.set("n", "<leader>ff", "<cmd>silent! !rustfmt $(echo %)<CR>")
+--
+-- [f]ormat [p]roject
+vim.keymap.set("n", "<leader>fp", "<cmd>silent! !cargo fmt<CR>")
