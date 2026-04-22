9b2faf8e06ab5e2a3cdb7b3f3d460d25cbc150c0
Jere Lantz <lantz.jere@gmail.com>
2026-04-22 20:53:31 +0300

    nvim fix

--- nvim/lua/konffit/plugins/telescope.lua
+++ nvim/lua/konffit/plugins/telescope.lua
@@ -11,7 +11,12 @@ config = function()
     require("telescope").setup{
         extensions = {
             fzf = {}
-        }
+        },
+        defaults = {
+            preview = {
+                treesitter = false,
+            },
+        },
     }
 
     require("telescope").load_extension("fzf")
